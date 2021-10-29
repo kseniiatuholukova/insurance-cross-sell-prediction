8848c8a3f2db539218b3bde6734a7b10f914057a
Kseniia Tuholukova <k.tuholukova@gmail.com>
2021-10-29 21:35:40 +0300

    Add normalization

--- app/utils/dataloader.py
+++ app/utils/dataloader.py
@@ -4,35 +4,36 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 
+def normalize(data):
+    normalized_data = (data - data.min()) / (data.max() - data.min())
+    return normalized_data
+
+
 class DataLoader():
     def fit(self, dataset):
         self.dataset = dataset.copy()
 
     def load_data(self):
-        def normalize(data):
-            normalized_data = (data - data.min()) / (data.max() - data.min())
-            return normalized_data
-
         self.dataset['Age'] = normalize(self.dataset['Age'])
-        self.dataset['Annual_Premium'] = normalize(
-            self.dataset['Annual_Premium'])
+        # self.dataset['Annual_Premium'] = normalize(
+        #     self.dataset['Annual_Premium'])
 
-        n_bins_vintage = (self.dataset['Vintage'].max() -
-                          self.dataset['Vintage'].min()) / 50
-        self.dataset['Vintage'] = pd.cut(self.dataset['Vintage'],
-                                         n_bins_vintage)
+        # n_bins_vintage = (self.dataset['Vintage'].max() -
+        #                   self.dataset['Vintage'].min()) / 50
+        # self.dataset['Vintage'] = pd.cut(self.dataset['Vintage'],
+        #                                  n_bins_vintage)
 
         le = LabelEncoder()
 
         le.fit(self.dataset['Gender'])
         self.dataset['Gender'] = le.transform(self.dataset['Gender'])
 
-        le.fit(self.dataset['Driving_Lisence'])
-        self.dataset['Driving_Lisence'] = le.transform(
-            self.dataset['Driving_Lisence'])
+        # le.fit(self.dataset['Driving_Lisence'])
+        # self.dataset['Driving_Lisence'] = le.transform(
+        #     self.dataset['Driving_Lisence'])
 
-        le.fit(self.dataset['Region_Code'])
-        self.dataset['Region_Code'] = le.transform(self.dataset['Region_Code'])
+        # le.fit(self.dataset['Region_Code'])
+        # self.dataset['Region_Code'] = le.transform(self.dataset['Region_Code'])
 
         le.fit(self.dataset['Previously_Insured'])
         self.dataset['Previously_Insured'] = le.transform(
@@ -49,9 +50,11 @@ class DataLoader():
         self.dataset['Policy_Sales_Channel'] = le.transform(
             self.dataset['Policy_Sales_Channel'])
 
-        le.fit(self.dataset['Vintage'])
-        self.dataset['Vintage'] = le.transform(self.dataset['Vintage'])
+        # le.fit(self.dataset['Vintage'])
+        # self.dataset['Vintage'] = le.transform(self.dataset['Vintage'])
 
-        self.dataset = self.dataset.drop('id', axis=1)
+        drop_elements = ['id', 'Driving_Lisence', 'Region_Code', 'Vintage']
+
+        self.dataset = self.dataset.drop(drop_elements, axis=1)
 
         return self.dataset
