86c4d2ad964e7b077b6a327d06901d91aa00d1a6
Kseniia Tuholukova <k.tuholukova@gmail.com>
2021-11-20 23:20:02 +0200

    Remove resampler
    Is not supported by the external validator

--- app/utils/trainer.py
+++ app/utils/trainer.py
@@ -1,14 +1,10 @@
 from sklearn.ensemble import RandomForestClassifier as RFC
 
-from utils.dataloader import Resampler
-
 
 class Estimator:
     @staticmethod
     def fit(train_x, train_y):
-        sampler = Resampler()
-        X_resampled, y_resampled = sampler.fit(train_x, train_y)
-        return RFC().fit(X_resampled, y_resampled)
+        return RFC().fit(train_x, train_y)
 
     @staticmethod
     def predict(trained, test_x):
